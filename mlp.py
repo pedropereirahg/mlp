77733e6902ac57ccba44275abb3fad701ef6da25
Adriano Augusto Satti Ortega Boschi <adriano.boschi@usp.br>
2017-04-29 23:52:41 +0000

    MLP working

--- mlp.py
+++ mlp.py
@@ -1,39 +1,26 @@
-###################################
-#   Multiplica-se os dados de entradas com os pesos A
-#   Aplica-se a funcao de ativacao f
-#   Multiplica-se os dados da camada escondida com os pesos B
-#   Aplica-se a funcao de ativacao g
-#   --------BACKPROPAGATION----------
-#   Calculamos o erro baseado na saida
-#   Multiplicamos o erro
-#   Soma-se as camadas escondidas
-#   Substituimos o valor na derivada f'
-#
-#######################################
 #http://mathesaurus.sourceforge.net/matlab-python-xref.pdf
 
 import numpy as np
 from scipy.special import expit
-#from decimal import Decimal
 
 def feed_foward(X,A,B,N):
     Zin = np.dot((np.append(np.ones((N,1)),X,1)),(np.transpose(A)))
-    #Z = expit(Zin)
-    Z = np.float64(1./(1+np.exp(-Zin)))
+    Z = expit(Zin)
+    #Z = np.float64(1./(1+np.exp(-Zin)))
     Yin = np.dot((np.append(np.ones((N,1)),Z,1)),(np.transpose(B)))
     Y= Yin # Nao tem segunda funcao de ativiacao, arrumar pro EP
     return Y
 
 def gradiente(X,d,A,B,N):
     Zin = np.dot((np.append(np.ones((N,1)),X,1)),(np.transpose(A)))
-    #print(Zin)
-    Z = np.float64(1./(1+np.exp(-Zin)))
-    #Z = expit(Zin)
+    #Z = np.float64(1./(1+np.exp(-Zin)))
+    Z = expit(Zin)
     Yin = np.dot((np.append(np.ones((N,1)),Z,1)),(np.transpose(B)))
     Y= Yin # Nao tem segunda funcao de ativiacao, arrumar pro EP
     erro = Y - d
     
-    grad_aux = np.dot((np.transpose(erro)),(np.append(np.ones((N,1)),Z,1))) # Nao tem segunda funcao de ativiacao, arrumar pro EP
+    grad_aux = np.dot((np.transpose(erro)),(np.append(np.ones((N,1)),Z,1))) 
+    # Nao tem segunda funcao de ativiacao, arrumar pro EP
     
     dJdB = (1./N) * (grad_aux)
     dJdZ = np.dot(erro,B)
@@ -45,7 +32,7 @@ def gradiente(X,d,A,B,N):
 
 def vetor_concat(a,b):
     a = np.reshape(a, (-1,1),'F')
-    b = np.reshape(a, (-1,1),'F')
+    b = np.reshape(b, (-1,1),'F')
     aux = np.concatenate((a, b), axis=0)
     return aux
     
@@ -57,9 +44,7 @@ def bis_mlp(X,d,A,B,dJdA,dJdB,N):
     
     Aaux = A - alfa_u*dJdA
     Baux = B - alfa_u*dJdB
-    print("merda 62")
     dJdAaux,dJdBaux = gradiente(X,d,Aaux,Baux,N)
-    #dJdAaux,dJdBaux = gradiente(X,d,np.around(Aaux,decimals=4),np.around(Baux,decimals=4),N)
 
     g = vetor_concat(dJdAaux,dJdBaux)
     hl = np.dot(np.transpose(g),dir)
@@ -68,16 +53,13 @@ def bis_mlp(X,d,A,B,dJdA,dJdB,N):
         alfa_u = 2*alfa_u
         Aaux = A - alfa_u*dJdA;
         Baux = B - alfa_u*dJdB;
-        print("merda 75")
         dJdAaux,dJdBaux = gradiente(X,d,Aaux,Baux,N)
-        #dJdAaux,dJdBaux = gradiente(X,d,np.around(Aaux,decimals=4),np.around(Baux,decimals=4),N)
         g = vetor_concat(dJdAaux,dJdBaux)
         hl = np.dot(np.transpose(g),dir)
 
     alfa_m = (alfa_l + alfa_u)/2
     Aaux = A - alfa_u*dJdA;
     Baux = B - alfa_u*dJdB;
-    print("merda 84")
     dJdAaux,dJdBaux = gradiente(X,d,Aaux,Baux,N)
 
     g = vetor_concat(dJdAaux,dJdBaux)
@@ -95,7 +77,6 @@ def bis_mlp(X,d,A,B,dJdA,dJdB,N):
         alfa_m = (alfa_l+alfa_u)/2;
         Aaux = A - alfa_m*dJdA;
         Baux = B - alfa_m*dJdB;   
-        print("merda 97")
         dJdAaux,dJdBaux = gradiente(X,d,Aaux,Baux,N)
         g = vetor_concat(dJdAaux,dJdBaux)
         hl = np.dot(np.transpose(g),dir)
@@ -124,13 +105,14 @@ B = np.random.rand(ns,(h+1))
 Y = feed_foward(X,A,B,N)
 erro = Y - d
 EQM = (1./N) * ((erro*erro).sum())
+
 i = 0
 alfa = 1
 
 vEQM = []
 vEQM.append(EQM)
 
-while (EQM > 1.0e-5 and i<100):
+while (EQM > 1.0e-5 and i<10000):
     i = i+1
     dJdA,dJdB = gradiente(X,d,A,B,N)
     alfa = bis_mlp(X,d,A,B,dJdA,dJdB,N)
@@ -140,6 +122,5 @@ while (EQM > 1.0e-5 and i<100):
     erro = Y - d
     EQM = (1./N) * ((erro*erro).sum())
     vEQM.append(EQM)
-    #print(Y)
 Y = feed_foward(X,A,B,N)
 print(Y)
