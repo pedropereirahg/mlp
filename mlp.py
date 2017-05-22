5a98da20c7e4b761b1dcc3f3f6133453e97b5610
Rodrigo Guerra <guerra@foxway.com.br>
2017-05-21 21:34:43 -0300

    MLP Done

--- mlp.py
+++ mlp.py
@@ -2,14 +2,13 @@
 
 import numpy as np
 from scipy.special import expit
-import os
-
+import matplotlib.pyplot as plt
 
 def feed_forward(x, a, b, n):
     z_in = np.dot((np.append(np.ones((n, 1)), x, 1)), (np.transpose(a)))
     z = expit(z_in)
     y_in = np.dot((np.append(np.ones((n, 1)), z, 1)), (np.transpose(b)))
-    y = y_in  # Nao tem segunda funcao de ativiacao, arrumar pro EP
+    y = expit(y_in)  # Nao tem segunda funcao de ativiacao, arrumar pro EP
     return y
 
 
@@ -17,12 +16,14 @@ def gradient(x, d, a, b, n):
     z_in = np.dot((np.append(np.ones((n, 1)), x, 1)), (np.transpose(a)))
     z = expit(z_in)
     y_in = np.dot((np.append(np.ones((n, 1)), z, 1)), (np.transpose(b)))
-    y = y_in  # Nao tem segunda funcao de ativiacao, arrumar pro EP
+    y = expit(y_in)  # Nao tem segunda funcao de ativiacao, arrumar pro EP
     error = y - d
 
-    grad_aux = np.dot((np.transpose(error)), (np.append(np.ones((n, 1)), z, 1))) # Nao tem segunda funcao de ativiacao, arrumar pro EP
+    grad_aux = np.dot((np.transpose(error * ((1 - y) * y))),
+                      (np.append(np.ones((n, 1)), z, 1)))  # Nao tem segunda funcao de ativiacao, arrumar pro EP
 
     d_jd_b = (1. / n) * grad_aux
+
     d_jd_z = np.dot(error, b)
     d_jd_z = np.delete(d_jd_z, 0, 1)
 
@@ -87,77 +88,45 @@ def bis_mlp(x, d, a, b, d_jd_a, d_jd_b, n):
 
 if __name__ == '__main__':
 
-    url_dataset = "dataset/"
-    url_test = "testes/"
-    url_learning = "treinamento/"
-    url_sample = "sample/"
+    input = "/Users/administrador/Dropbox/EACH/IA/Clodoaldo/ia-photos-transform/PCP-16 CPB-8/sample/HOG_sample/train_5a_00001.txt"
+    #if input in os.environ:
+    X = np.loadtxt(input)
+    X = X.reshape(1, len(X))
+    d = np.array([[0, 1, 0]])
+    aux = np.shape(X)
+    h = 3
+    N = aux[0]
+    ne = aux[1]
+    aux = np.shape(d)
+    ns = aux[1]
 
-    pixels_per_celL = 8
-    cells_per_block = 1
-    orientations = 9
-    run = ",,"
+    A = np.random.rand(h, (ne + 1))
+    B = np.random.rand(ns, (h + 1))
 
-    if "PIXELS_PER_CELL" in os.environ:
-        pixels_per_celL = int(os.environ["PIXELS_PER_CELL"])
+    # Feedfoward para a saida
+    Y = feed_forward(X, A, B, N)
+    error = Y - d
+    EQM = (1. / N) * ((error * error).sum())
 
-    if "CELLS_PER_BLOCK" in os.environ:
-        cells_per_block = int(os.environ["CELLS_PER_BLOCK"])
+    i = 0
+    alfa = 1
 
-    if "ORIENTATIONS" in os.environ:
-        orientations = int(os.environ["ORIENTATIONS"])
+    vEQM = []
+    vEQM.append(EQM)
 
-    if "RUN" in os.environ:
-        run = os.environ["RUN"]
+    while EQM > 1.0e-5 and i < 10000:
+        i = i + 1
+        dJdA, dJdB = gradient(X, d, A, B, N)
+        alfa = bis_mlp(X, d, A, B, dJdA, dJdB, N)
+        A = A - alfa * dJdA
+        B = B - alfa * dJdB
+        Y = feed_forward(X, A, B, N)
+        error = Y - d
+        EQM = (1. / N) * ((error * error).sum())
+        vEQM.append(EQM)
+    Y = feed_forward(X, A, B, N)
+    print(Y)
 
-    run = run.split(",")
+    print(vEQM)
 
-    url_result = "build/PCP-" + repr(pixels_per_celL) + "-CPB-" + repr(cells_per_block) + "/"
-
-    for layer in run:
-        # Set path
-        os.chdir(url_result + layer.lower() + "/HOG_" + layer.lower())
-
-        for file_name in os.listdir("."):
-            f = open(file_name, 'r')
-            X = [map(float, line.split(',')) for line in f]
-
-            # Set X, d , H
-            # X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
-            d = np.array([[0, 0, 1], [0, 1, 0], [1, 0, 0]])
-            h = 3
-
-            # Cria N, ne e ns
-            aux = np.shape(X)
-            N = aux[0]
-            ne = aux[1]
-            aux = np.shape(d)
-
-            ns = aux[1]
-
-            # Cria os pesos aleatorios para A e B
-            A = np.random.rand(h, (ne + 1))
-            B = np.random.rand(ns, (h + 1))
-
-            # Feedfoward para a saida
-            Y = feed_forward(X, A, B, N)
-            error = Y - d
-            EQM = (1. / N) * ((error * error).sum())
-
-            i = 0
-            alfa = 1
-
-            vEQM = []
-            vEQM.append(EQM)
-
-            while EQM > 1.0e-5 and i < 10000:
-                i = i + 1
-                dJdA, dJdB = gradient(X, d, A, B, N)
-                alfa = bis_mlp(X, d, A, B, dJdA, dJdB, N)
-                A = A - alfa * dJdA
-                B = B - alfa * dJdB
-                Y = feed_forward(X, A, B, N)
-                error = Y - d
-                EQM = (1. / N) * ((error * error).sum())
-                vEQM.append(EQM)
-            Y = feed_forward(X, A, B, N)
-            print(Y)
+    print(np.argmax(Y))
