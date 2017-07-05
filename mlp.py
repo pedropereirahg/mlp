02beb2d168e8c528e87f6713df03a001288fd66e
pedrogoncalvesk <pedrogoncalvesp.95@gmail.com>
2017-07-05 05:51:02 -0300

    Atualiza o modulo do python

--- mlp.py
+++ mlp.py
@@ -6,9 +6,11 @@ import sys
 
 def mlp(args):
     if len(args) > 2:
-        os.system("octave -p " + sys.prefix + "/mlp/ startup.m " + str(args[0]) + " " + str(args[1]) + " " + str(args[2]))
+        os.system("octave -p " + sys.prefix + "/mlp" + sys.prefix + "/mlp/startup.m " +
+                  str(args[0]) + " " + str(args[1]) + " " + str(args[2]))
     else:
-        os.system("octave -p " + sys.prefix + "/mlp/ startup.m " + str(args[0]) + " " + str(args[1]))
+        os.system("octave -p " + sys.prefix + "/mlp " + sys.prefix + "/mlp/startup.m " +
+                  str(args[0]) + " " + str(args[1]))
 
 if __name__ == "__main__":
     mlp(sys.argv[1:])
