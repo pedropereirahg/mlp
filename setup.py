6c29ea40b2b63dcff7720878ab95cab065f8ce07
pedrogoncalvesk <pedrogoncalvesp.95@gmail.com>
2017-07-05 03:33:31 -0300

    Bugfix: Adding octave files

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-octaveFiles = glob.glob('./*.m')
+octaveFiles = glob('./*.m')
 
 setup(
     name='mlp',
